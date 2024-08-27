c8db5426776c5af45b65c8b618886ec7e57c48bb
genmzy <986299679@qq.com>
2024-08-27 17:11:20 +0800

    feat: backup add fantasquesans nerd font version 3; cover all proxy environment variables when start clash

--- .config/scripts/startup_clash.sh
+++ .config/scripts/startup_clash.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd ~/Backup/clash-for-linux-master/
 
+ps axu|grep clash| grep -v grep
+
 clash_count=`ps axu| grep clash | grep -v "startup_clash" | grep -v grep | wc -l`
 echo "clash count: "$clash_count
 
@@ -8,6 +10,6 @@ if [ $clash_count -gt 0 ]
 then
 	/usr/bin/echo "Clash already started."
 else
-	ALL_PROXY="" https_proxy="" http_proxy="" ./start.sh
+	ALL_PROXY="" all_proxy="" HTTPS_PROXY=""  https_proxy="" HTTP_PROXY="" http_proxy=""  ./start.sh
 fi
 
