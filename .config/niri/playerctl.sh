e479bda2d4e7b8e962c8f051148038db7fe01856
genmzy <986299679@qq.com>
2026-08-03 10:04:33 +0800

    feat(niri): add playerctl stop/start notify

--- .config/niri/playerctl.sh
+++ .config/niri/playerctl.sh
@@ -16,6 +16,8 @@ done < <(playerctl -l)
 if [ ${#active_players[@]} -eq 1 ]; then
     target="${active_players[-1]}" # -1:latest 0:oldest
     playerctl --player="$target" "$op"
+    notify-send --app-name="$target" 'Sound Stopped'
 else
     playerctl "$op"
+    notify-send --app-name="playerctl" 'Sound Started'
 fi
