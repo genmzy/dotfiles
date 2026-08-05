d9e0070b6ae28097e886458fc7ff515dfb799a40
genmzy <986299679@qq.com>
2026-08-05 08:42:29 +0800

    feat(niri): switch volume and video (Mod+)F-area for convenience and small notification for playerctl

--- .config/niri/playerctl.sh
+++ .config/niri/playerctl.sh
@@ -5,19 +5,23 @@ if [ ${#1} -gt 0 ]; then
     op=$1
 fi
 
+all=0
 active_players=()
 while read -r player; do
     status=$(playerctl --player="$player" status 2>/dev/null)
     if [ "$status" = "Playing" ]; then
         active_players+=("$player")
     fi
+    ((all += 1))
 done < <(playerctl -l)
 
 if [ ${#active_players[@]} -eq 1 ]; then
     target="${active_players[-1]}" # -1:latest 0:oldest
     playerctl --player="$target" "$op"
-    notify-send --app-name="$target" 'Sound Stopped'
+    notify-send --app-name="$target" "Sound Stopped"
+elif [ "$all" -eq 0 ]; then
+    notify-send --app-name="playerctl" "No Player Instance"
 else
     playerctl "$op"
-    notify-send --app-name="playerctl" 'Sound Started'
+    notify-send --app-name="playerctl" "Sound Started"
 fi
